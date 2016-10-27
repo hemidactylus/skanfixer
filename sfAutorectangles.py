0456b07c801861d5294694822a80bea3d9360902
hemidactylus <lumidoro@gmail.com>
2016-10-27 19:33:04 +0200

    Started working on the rectangle merge (suboptimal) routine

--- sfAutorectangles.py
+++ sfAutorectangles.py
@@ -8,6 +8,8 @@ import numpy as np
 from time import time
 from scipy import ndimage as nd
 
+from sfPoint import sfPoint
+
 def handleImage(image):
     # (resize and) convert to grayscale, no alpha channel
     greyimage=image.resize((300,420),Image.ANTIALIAS).convert('L')
@@ -32,19 +34,49 @@ def handleImage(image):
     MIN_LABEL_SIZE=1000
     print 'RESULT: %s' % (labelsizes)
     print 'Labels: ', labels
-    goodregions=[]
+    regions=[]
     for rlabel in range(1,labels+1):
         print 'LAB ', rlabel
         if labelsizes[rlabel] >= MIN_LABEL_SIZE:
             # find extrema of this region with a trick
-            theseXs=np.where(np.any(laberoded==rlabel,axis=1))[0]
-            theseYs=np.where(np.any(laberoded==rlabel,axis=0))[0]
+            theseXs=np.where(np.any(laberoded==rlabel,axis=0))[0]
+            theseYs=np.where(np.any(laberoded==rlabel,axis=1))[0]
             xmin=theseXs[0]
             xmax=theseXs[-1]
             ymin=theseYs[0]
             ymax=theseYs[-1]
             print 'Label %i => (%i,%i) to (%i,%i)' % (rlabel,xmin,ymin,xmax,ymax)
+            regions.append((sfPoint(xmin,ymin),sfPoint(xmax,ymax)))
 
+    # SIMULATE SOMETHING TO DO
+    regions.append((sfPoint(150,320),sfPoint(214,413))) # this overlaps with the last real one
+
+    print 'REGIONS BEFORE OVERLAP CHECK:\n', '\n'.join('    %s ==> %s' % reg for reg in regions)
+
+    # here each rectangle is added/merged onto a stack so that if
+    # a new rectangle to insert has some overlap to an existing one they become a larger rectangle.
+    # This has to be iterated until there are no merging anymore.
+    # Performance is no big deal (there aren't many rectangles at this point), but there has to be
+    # a more elegant and less wasting solution
+    startRegions=regions
+    mergedRegions=[]
+    while len(startRegions)!=len(mergedRegions):
+        startRegions=mergedRegions
+        mergedRegions=merge_rectangles(mergedRegions)
+
+    print 'REGIONS AFTER MERGING:\n', '\n'.join('    %s ==> %s' % reg for reg in regions)
+
+def merge_rectangles(srcRegions):
+    '''
+
+        Given an input list of 2-uples containing sfPoints which are the left-top-most and the right-bottom-most
+        corner of a rectangle each, it reduces their number by merging any two who happend to overlap.
+        The merge is done onto the smallest rectangle containing both.
+
+        To Be Implemented
+    '''
+    raise NotImplementedError
+    return srcRegions
 
 if __name__=='__main__':
     print 'Testing'
